ca58023e1b2c3ff963c5756d4d612fd53b432b97
Ariane <ariane_drouin@outlook.com>
2023-03-17 21:36:26 -0400

    font

--- UnderwaterCity/UnderwaterCity/MauiProgram.cs
+++ UnderwaterCity/UnderwaterCity/MauiProgram.cs
@@ -13,6 +13,7 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("AtlantislnlineGrunge.ttf", "Atlantis");
 			});
         builder.Services.AddSingleton<MainPage>();
         builder.Services.AddSingleton<MainViewModel>();
