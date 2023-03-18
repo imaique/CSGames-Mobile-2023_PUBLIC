b5b5052bcc84c1e4bb46ae70443591f0b6e83d5c
Michael <osujim2012@gmail.com>
2023-03-17 20:15:29 -0400

    Update UnderwaterServices.cs

--- UnderwaterCity/UnderwaterCity/Services/UnderwaterServices.cs
+++ UnderwaterCity/UnderwaterCity/Services/UnderwaterServices.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text.Json;
+
 namespace UnderwaterCity.Services
 {
 	public static class UnderwaterServices
@@ -14,12 +16,26 @@ namespace UnderwaterCity.Services
 			};
 		}
 
-		public static async Task<IEnumerable<String>> GetTemp()
+		public static async Task<WeatherForecast> GetTemp()
 		{
 			var json = await client.GetStringAsync("temp");
-			Console.WriteLine(json);
-			return null;
+			WeatherForecast weather = JsonSerializer.Deserialize<WeatherForecast>(json);
+
+            Console.WriteLine(weather.information.ext_water_temp);
+			return weather;
 		}
+
+
+    }
+    public class WeatherForecast
+    {
+        public class WeatherInfo
+        {
+            public float ext_water_temp { get; set; }
+            public float int_temp { get; set; }
+        }
+        public WeatherInfo information { get; set; }
+
     }
 }
 
